3cec986e0f2ff5af9959f478008013aee797b586
unknown <adncpamarketing@yahoo.com>
2023-11-12 10:58:58 -0800

    added javascript

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-console.log(body);
 
+function setGradient(){
+	body.style.background = "linear-gradient(to right, "	
+	+ color1.value	
+	+ ","	
+	+ color2.value	
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
